074cc29516b164725a9a876d90f7b363db2fa5c3
Bwat-dev <tech@bwat.fr>
2021-10-29 10:42:59 +0200

    add resource resolver

--- Http/Controllers/CategoryController.php
+++ Http/Controllers/CategoryController.php
@@ -4,7 +4,9 @@ namespace App\Twill\Capsules\Categories\Http\Controllers;
 
 use A17\Twill\Http\Controllers\Admin\ModuleController;
 use App\Twill\Capsules\Menus\Models\Menu;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 
 class CategoryController extends ModuleController
 {
@@ -26,10 +28,7 @@ class CategoryController extends ModuleController
 
     protected function formData($request)
     {
-        $resource = $this->getResource();
-        return [
-            'resource' => $resource
-        ];
+        return $this->indexData($request);
     }
 
     protected function filterScope($prepend = [])
@@ -40,8 +39,35 @@ class CategoryController extends ModuleController
 
     protected function getResource()
     {
-        $url = $this->request->route()->uri;
-        return Arr::first(explode('/', $url));
+        $routeName = $this->request->route()->getName();
+
+        $activeMenus = explode('.', $routeName);
+
+        //starts at 1 because first segment of all back route name is 'admin'
+        $global_active_navigation = $activeMenus[1];
+
+        $singularName = Str::singular($global_active_navigation);
+        $modelClass = config('twill.namespace') . '\\Models\\' . Str::studly($singularName);
+
+        if(!class_exists($modelClass)) {
+            $modelClass = $this->getCapsuleByModule($global_active_navigation)['model'];
+        }
+
+        if(!class_exists($modelClass)) {
+            return $global_active_navigation;
+        }
+
+        $morph = Arr::where(Relation::$morphMap, function ($value) use($modelClass) {
+            return $value == $modelClass;
+        });
+
+        $morph = array_key_first($morph);
+
+        if($morph) {
+            return $morph;
+        }
+
+        return (new $modelClass)->getTable();
     }
 
 }
--- routes/admin.php
+++ routes/admin.php
@@ -1,3 +1,2 @@
 <?php
 
-//Route::module('categories');
